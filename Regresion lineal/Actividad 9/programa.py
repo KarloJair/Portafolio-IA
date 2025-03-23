40d554a0655efbe54e44a9a9c03c1be66cbb69c8
Karlo Jair <karlo-sonic1@hotmail.com>
2025-03-22 18:03:34 -0600

    Actividad 9

--- Regresion lineal/Actividad 9/programa.py
+++ Regresion lineal/Actividad 9/programa.py
@@ -1,13 +1,6 @@
 # Imports necesarios
 import numpy as np
 import pandas as pd
-import seaborn as sb
-import matplotlib.pyplot as plt
-#%matplotlib inline
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -23,19 +16,17 @@ dataX = filtered_data[["Word count"]]
 X_train=np.array(dataX)
 y_train=filtered_data['# Shares'].values
  
-#Creamoselobjetode RegresiónLinear
 regr=linear_model.LinearRegression()
  
-  #Entrenamosnuestromodelo
 regr.fit(X_train,y_train)
 
-#Hacemoslasprediccionesqueen definitivauna línea(enestecaso,alser2D)
+#Hacemos las predicciones que en definitiva una línea(en este caso,alser2D)
 y_pred=regr.predict(X_train)
-#Veamosloscoeficienetesobtenidos,Ennuestro caso,seránlaTangente
+#Veamos los coeficienetes obtenidos,En nuestro caso,serán la Tangente
 print('Coefficients:\n',regr.coef_)
-#Esteeselvalordonde cortael ejeY(enX=0)
+#Este es el valor donde corta el ejeY(enX=0)
 print('Independent term:\n',regr.intercept_)
-#ErrorCuadradoMedio
+#Error Cuadrado Medio
 print("Mean squared error:%.2f"%mean_squared_error(y_train,y_pred))
-#PuntajedeVarianza. Elmejorpuntajeesun1.0
+#Puntaje de Varianza. El mejor puntaje es un 1.0
 print('Variance score:%.2f'%r2_score(y_train,y_pred))
